62cdf493fbe2a3cdbd0ca83cbcc68e9ab6ef5d98
Anna <annaproxy@protonmail.com>
2020-05-20 20:47:43 +0200

    Clean up metalearn_train and finetune_train

--- finetune_train.py
+++ finetune_train.py
@@ -2,11 +2,14 @@
 from get_language_dataset import get_language_dataset
 from i_hate_params_i_want_them_all_in_one_file import get_params
 from allennlp.models.model import Model
-
+import os
 import torch
 from torch.optim import Adam
 from torch import autograd
 import numpy as np 
+from udify import util 
+import json 
+import subprocess
 
 training_tasks = []
 training_tasks.append(get_language_dataset('UD_Italian-ISDT','it_isdt-ud'))
@@ -17,20 +20,32 @@ training_tasks.append(get_language_dataset('UD_Hindi-HDTB','hi_hdtb-ud'))
 training_tasks.append(get_language_dataset('UD_Korean-Kaist','ko_kaist-ud'))
 training_tasks.append(get_language_dataset('UD_Arabic-PADT','ar_padt-ud'))
 
-print("All Data Loaded")
+validation_test_set = "data/expmix/" + "UD_Bulgarian-BTB" + "/" + "bg_btb-ud" + "-dev.conllu"
 
+print("All Data Loaded")
 train_params = get_params()
 
-SAVE_EVERY=40
+SAVE_EVERY = 10
+EPISODES = 400
+LR = 5e-5
+patience = 3
+warmup_steps = 50
+
+MODEL_SAVE_NAME = "finetune_5e5"
+MODEL_VAL_DIR = MODEL_SAVE_NAME + "VAL"
 Path_model = "logs/english_expmix_deps/2020.05.17_01.08.52/" #'./best.th'
 
 model = Model.load(train_params, Path_model).cuda()
 model.train()
 
-optimizer =  Adam(model.parameters(), 5e-5)
+optimizer =  Adam(model.parameters(), LR)
 losses = []; task_num_tokens_seen = np.zeros(len(training_tasks))
 
-for i, episode in enumerate(range(160)):
+best_validation_LAS = 0.0
+best_iteration = -9
+best_filename = None
+
+for i, episode in enumerate(range(EPISODES)):
     for j, task in enumerate(training_tasks):
         input_set = next(task)[0]
         loss = model(**input_set)['loss']
@@ -40,15 +55,45 @@ for i, episode in enumerate(range(160)):
         loss.backward()
         optimizer.step()
         
-    if (i+1) % 10 == 0:
-        print('Loss: %.3f' % np.mean(losses[-100:]))
-            
     if (i+1) % SAVE_EVERY == 0:
-        torch.save(model.state_dict(),"modellowlr_checkpoint_{}.th".format(i+1))
-        print("Model #{} has been saved.".format(i+1))
-        
+        print('Loss: %.3f' % np.mean(losses[-100:]))
 
+        SAVE_FILENAME = "best.th"
+        torch.save(model.state_dict(), os.path.join(MODEL_VAL_DIR, SAVE_FILENAME))
+        current_pred_file = os.path.join(MODEL_VAL_DIR,'predictions', "temp_val.conllu")
+        current_output_file = os.path.join(MODEL_VAL_DIR,'performance', "temp_performance" + str(i) + ".json")
 
+        util.predict_and_evaluate_model(
+            "udify_predictor",
+            train_params,
+            MODEL_VAL_DIR,
+            validation_test_set,
+            current_pred_file,
+            current_output_file,
+            batch_size=16
+        )
+        # Easiest way is just to fucking die and dump it to json and then read it.
+        with open(current_output_file, 'r') as f:
+            performance_dict = json.load(f)
+            val_LAS = performance_dict['LAS']['aligned_accuracy']
+            print(val_LAS)
+
+        if val_LAS > best_validation_LAS:
+            best_validation_LAS = val_LAS
+            best_iteration = i
+            patience = 5
+            # Remove previous best
+            if best_filename is not None:
+                subprocess.run(["rm", best_filename ])
+
+            # Save next best model 
+            backup_path = os.path.join(MODEL_VAL_DIR, "model" + str(i) + ".th")
+            torch.save(model.state_dict(), backup_path)
+            best_filename = backup_path
+        else:
+            patience -= 1
+            
+print("Best iteration:", best_iteration, best_filename)
 print("Success, Epoch's Last iteration loss: {}".format(losses[-1]))
 normalized_tokens_seen = task_num_tokens_seen / np.max(task_num_tokens_seen)
 print("Number of Tokens seen per task: {}, relative to maximum: {}".format(task_num_tokens_seen, normalized_tokens_seen))
--- metalearn_train.py
+++ metalearn_train.py
@@ -1,33 +1,58 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+sys.stdout.reconfigure(encoding='utf-8')
 """
 This file Meta-Trains on 7 languages
 And validates on Bulgarian
 """
+from allennlp.models.archival import archive_model
 from get_language_dataset import get_language_dataset
 from i_hate_params_i_want_them_all_in_one_file import get_params
 from allennlp.models.model import Model
 from udify.predictors import predictor
 from learn2learn.algorithms import MAML
-
+import numpy as np
 import torch
 from torch.optim import Adam
 from torch import autograd
+import subprocess
+from udify import util
+import json
+import subprocess
 
 training_tasks = []
-#training_tasks.append(get_language_dataset('UD_Italian-ISDT','it_isdt-ud'))
-#training_tasks.append(get_language_dataset('UD_Norwegian-Nynorsk','no_nynorsk-ud'))
-#training_tasks.append(get_language_dataset('UD_Czech-PDT','cs_pdt-ud'))
-#training_tasks.append(get_language_dataset('UD_Russian-SynTagRus','ru_syntagrus-ud'))
-#training_tasks.append(get_language_dataset('UD_Hindi-HDTB','hi_hdtb-ud'))
-#training_tasks.append(get_language_dataset('UD_Korean-Kaist','ko_kaist-ud'))
+training_tasks.append(get_language_dataset('UD_Italian-ISDT','it_isdt-ud'))
+training_tasks.append(get_language_dataset('UD_Norwegian-Nynorsk','no_nynorsk-ud'))
+training_tasks.append(get_language_dataset('UD_Czech-PDT','cs_pdt-ud'))
+training_tasks.append(get_language_dataset('UD_Russian-SynTagRus','ru_syntagrus-ud'))
+training_tasks.append(get_language_dataset('UD_Hindi-HDTB','hi_hdtb-ud'))
+training_tasks.append(get_language_dataset('UD_Korean-Kaist','ko_kaist-ud'))
 training_tasks.append(get_language_dataset('UD_Arabic-PADT','ar_padt-ud'))
-validation_iterator = get_language_dataset("UD_Bulgarian-BTB", "bg_btb-ud", True)
+
+# Get some samples from train set
+validation_iterator = get_language_dataset("UD_Bulgarian-BTB", "bg_btb-ud")
+validation_test_set = "data/expmix/" + "UD_Bulgarian-BTB" + "/" + "bg_btb-ud" + "-dev.conllu"
 
 print("All Data Loaded")
 
+SAVE_EVERY=10
+EPISODES = 200
 INNER_LR = 1e-4
 META_LR = 5e-5
+patience = 3
+warmup_steps = 50
+
 MODEL_FILE = "logs/english_expmix_deps/2020.05.17_01.08.52/"
-MODEL_SAVE_NAME = "metalearn"
+MODEL_SAVE_NAME = "metalearn_5e5"
+MODEL_VAL_DIR = MODEL_SAVE_NAME + "VAL"
+
+if not os.path.exists(MODEL_VAL_DIR):
+    subprocess.run(["mkdir", MODEL_VAL_DIR])
+    subprocess.run(["mkdir", MODEL_VAL_DIR + "/performance"])
+    subprocess.run(["mkdir", MODEL_VAL_DIR + "/predictions"])
+    subprocess.run(["cp", "-r", MODEL_FILE +"/vocabulary", MODEL_VAL_DIR])
+    subprocess.run(["cp", MODEL_FILE +"/config.json", MODEL_VAL_DIR])
 
 train_params = get_params()
 m = Model.load(train_params, MODEL_FILE,).cuda()
@@ -37,41 +62,112 @@ optimizer =  Adam(meta_m.parameters(), META_LR)
 with open("some_results.txt", "w") as f:
     f.write("Model ready\n")
 
-best_validation_las = 0.0
-for iteration in range(100):
+with open("val_las.txt", "w") as f:
+    f.write("Model ready\n")
+
+best_validation_LAS = 0.0
+best_iteration = -9
+best_filename = None
+
+
+task_num_tokens_seen = np.zeros(len(training_tasks))
+
+for iteration in range(EPISODES):
     iteration_loss = 0.0
-    for task_generator in training_tasks:
+
+    """Inner adaptation loop"""
+    for j, task_generator in enumerate(training_tasks):
         learner = meta_m.clone()
         support_set = next(task_generator)[0]
         query_set = next(task_generator)[0]
         inner_loss = learner.forward(**support_set)['loss']
-        print("\tone forward loss: ", inner_loss.item())
+        print("\t", inner_loss.item())
         learner.adapt(inner_loss, first_order=True)
         eval_loss = learner.forward(**query_set)['loss']
+
+        # Bookkeeping
+        task_num_tokens_seen[j] += len(support_set['tokens']['tokens'][0])
+        task_num_tokens_seen[j] += len(query_set['tokens']['tokens'][0])
         iteration_loss += eval_loss
         del eval_loss 
         del inner_loss 
         del support_set 
         del query_set
         del learner
+
+    # Sum up and normalize over all 7 losses
     iteration_loss /= len(training_tasks)
     optimizer.zero_grad()
     iteration_loss.backward()
     optimizer.step()
-    print("meta", iteration_loss.item())
+
+    # Bookkeeping
+    print(iteration, "meta", iteration_loss.item())
     with open("some_results.txt", "a") as f:
-        f.write("meta " + str(iteration_loss.item()))
+        f.write(str(iteration) + " meta " + str(iteration_loss.item()))
         f.write("\n")
     del iteration_loss 
-    with torch.no_grad():
-        if iteration+1 % 10 == 0:
-            udify_predictor = predictor.UdifyPredictor(meta_m.module, validation_iterator)
-            for batch in validation_iterator:
-                preds = udify_predictor.predict_batch_instance(batch)
-                print(preds, type(preds))
-                raise ValueError("Hi")
-            torch.save(meta_m.module.state_dict(), MODEL_SAVE_NAME + "_" + iteration + ".th")
 
+    # Meta validation
+    if (iteration+1) % SAVE_EVERY == 0:
+        # Do update
+        vallearner = meta_m.clone()
+        batch = next(validation_iterator)
+        print("Batch", batch)
+        metaval_loss = vallearner.forward(**batch[0])['loss']
+        vallearner.adapt(metaval_loss, first_order=True)
+
+        # Save model, this is necessary for prediction
+        SAVE_FILENAME = "best.th"
+        torch.save(vallearner.module.state_dict(), os.path.join(MODEL_VAL_DIR, SAVE_FILENAME))
+        archive_model(MODEL_VAL_DIR, files_to_archive=train_params.files_to_archive, archive_path =MODEL_VAL_DIR)
+
+        # Predict entire validation set
+        current_pred_file = os.path.join(MODEL_VAL_DIR,'predictions', "temp_val.conllu")
+        current_output_file = os.path.join(MODEL_VAL_DIR,'performance', "temp_performance" + str(iteration) + ".json")
+        util.predict_and_evaluate_model(
+            "udify_predictor",
+            train_params,
+            MODEL_VAL_DIR,
+            validation_test_set,
+            current_pred_file,
+            current_output_file,
+            batch_size=16
+        )
+
+        # Easiest way is just to fucking die and dump it to json and then read it.
+        with open(current_output_file, 'r') as f:
+            performance_dict = json.load(f)
+            val_LAS = performance_dict['LAS']['aligned_accuracy']
+            with open("val_las.txt","a") as f:
+                f.write(str(val_LAS))
+                f.write('\n')
+
+        # Check if we are doing better!
+        if val_LAS > best_validation_LAS:
+            best_validation_LAS = val_LAS
+            best_iteration = iteration
+            patience = 3
+            # Remove previous best
+            subprocess.run(["rm", best_filename ])
+
+            # Save META model for later inference
+            backup_path = os.path.join(MODEL_VAL_DIR, "model" + str(iteration) + ".th")
+            torch.save(meta_m.module.state_dict(), backup_path)
+            best_filename = backup_path
+        else:
+            patience -= 1
+
+        del vallearner
+        del metaval_loss
+        del batch
+        del val_LAS
+    if patience == 0 and iteration > warmup_steps:
+        print("Patience ran out, quitting", iteration)
+
+normalized_tokens_seen = task_num_tokens_seen / np.max(task_num_tokens_seen)
+print("Number of Tokens seen per task: {}, relative to maximum: {}".format(task_num_tokens_seen, normalized_tokens_seen))
+np.save('task_num_tokens_seen.npy', task_num_tokens_seen)
 
 
 
