fb692805fb30d7d34ea42febd5b4bf17c25ebcff
Anna <annaproxy@protonmail.com>
2020-05-31 20:45:06 +0200

    Removing some more unnecessary files

--- pretrain_english.py
+++
@@ -1,6 +0,0 @@
-# Manual Pipeline
-# Collect all datasets in one directory (manually done)
-# Concat them and build vocabulary (concat_treebanks.py, train.py with valueerror)
-# Point the train_english_only to correct vocabulary 
-# Zero-shot prediction with predict_and_evaluate
-
